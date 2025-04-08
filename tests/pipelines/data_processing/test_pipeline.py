077342eec59cb0de3d3e18ccacdb9c16efb77888
DeliciousBoy <outdata.gide@gmail.com>
2025-04-08 12:55:10 +0700

    Refactor test_clean_text function and update parameterized tests for improved coverage

--- tests/pipelines/data_processing/test_pipeline.py
+++ tests/pipelines/data_processing/test_pipeline.py
@@ -1,18 +1,33 @@
 import pytest
-
 from llm_chatbot_backend.pipelines.data_processing.nodes import (
     build_rag_context,
+    clean_text,
+    process_text,
 )
 
 
-@pytest.fixture()
-def test_build_rag_context():
-    data = {
-        "forum_text": "ฉันมีอาการปวดหัว",
-        "doctor_reply": "คุณควรพักผ่อน",
-        "disease_text": "ไมเกรน",
-        "tags": ["ปวดหัว", "ไมเกรน"],
-    }
-    result = build_rag_context(data)
-    assert "[คำถามจากผู้ใช้]" in result
-    assert "[คำตอบจากแพทย์]" in result
+@pytest.mark.parametrize(
+    "text,expected",
+    [
+        ("มากกก", "มากก"),  # ลดอักษรไทยซ้ำ
+        ("https://example.com", ""),  # ลบ URL
+        ("เศร้าา🥲", "เศร้า"),  # ลบ emoji
+        ("   ", ""),  # ลบ whitespace
+    ],
+)
+def test_clean_text(text, expected):
+    assert clean_text(None) == ""
+    assert clean_text(text) == expected
+
+
+# @pytest.fixture()
+# def test_build_rag_context():
+#     data = {
+#         "forum_text": "ฉันมีอาการปวดหัว",
+#         "doctor_reply": "คุณควรพักผ่อน",
+#         "disease_text": "ไมเกรน",
+#         "tags": ["ปวดหัว", "ไมเกรน"],
+#     }
+#     result = build_rag_context(data)
+#     assert "[คำถามจากผู้ใช้]" in result
+#     assert "[คำตอบจากแพทย์]" in result
